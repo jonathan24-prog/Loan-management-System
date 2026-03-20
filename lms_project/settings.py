68ebbcde542b432d396756573eda5696a23c7ee7
Jonathan <jonathansumalpong24@gmail.com>
2026-03-20 19:55:42 +0800

    updated

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -55,17 +55,25 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 # ---------- DATABASE ----------
 
 # Use external PostgreSQL (Supabase/ElephantSQL)
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get(
-            "DATABASE_URL",
-            "postgresql://lms_db_uk3v_user:njTe4hdgPZw3VJb2Ycd4k15ffbFkxF1h@dpg-d6tusi94tr6s73bthbjg-a.oregon-postgres.render.com/lms_db_uk3v"  # replace with your free DB URL if not using env
+# DATABASES = {
+#     "default": dj_database_url.parse(
+#         os.environ.get(
+#             "DATABASE_URL",
+#             "postgresql://lms_db_uk3v_user:njTe4hdgPZw3VJb2Ycd4k15ffbFkxF1h@dpg-d6tusi94tr6s73bthbjg-a.oregon-postgres.render.com/lms_db_uk3v"  # replace with your free DB URL if not using env
            
-        ),
-        conn_max_age=600,
-        ssl_require=True
-    )
+#         ),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # ---------- PASSWORD VALIDATION ----------
 AUTH_PASSWORD_VALIDATORS = [

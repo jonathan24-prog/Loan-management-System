190e7baedc3088a7f6b9778df15a63a89e2ca8a9
Jonathan <jonathansumalpong24@gmail.com>
2026-06-07 21:45:55 +0800

    settings adjustments

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -9,7 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-fallback-key")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = [
+    "174.138.40.4",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://174.138.40.4",
+]
 
 # Application definition
 INSTALLED_APPS = [
